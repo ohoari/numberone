cce07f86402dca88a7b46adf4c7030a237744e3a
unknown <arso2018@icloud.com>
2021-07-08 20:19:48 +0900

    ohoari

--- lib/main.dart
+++ lib/main.dart
@@ -94,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'い',
+              'ohoari',
             ),
             Text(
               '$_counter',
